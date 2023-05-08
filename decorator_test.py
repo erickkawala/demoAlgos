b32bab7713d1cb9c6938e7b2cc766d50a5fd82d8
erickkawala <erickkawala@gmail.com>
2023-05-08 15:58:22 -0500

    Update decorator_test.py
    comment in wrong place, prevented console output

--- decorator_test.py
+++ decorator_test.py
@@ -10,10 +10,11 @@ def run(t_str):
 [[1, 11, 12],
 [13, 14, 15],
 [16, 17, 18]]
-"""    
+  
     t_str += decorate()
     t_str += decorate2()
     print(t_str)
+"""  
     matrix[1][1][1]=str(matrix[1][1][1])
     matrix[1][1][1]=t_str
     #print(matrix[1][1][1])
